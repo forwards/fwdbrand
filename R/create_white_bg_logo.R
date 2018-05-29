d4f956b25995af902fca4f2d8423c12665d28f0b
Maëlle Salmon <maelle@itsalocke.com>
2018-05-29 13:17:49 +0200

    start repo cleanup

--- R/create_white_bg_logo.R
+++ R/create_white_bg_logo.R
@@ -1,5 +1,5 @@
 library("magrittr")
-logo_path <- "https://github.com/forwards/website_source/raw/master/static/images/forwards.png"
+logo_path <- "assets/forwards.png"
 
 # replace transparent background with white background
 # note, this is probably not the best solution
@@ -7,4 +7,4 @@ logo <- magick::image_read(logo_path)
 height <- magick::image_info(logo)$height
 magick::image_blank(height, height, color = "white") %>%
   magick::image_composite(logo) %>%
-  magick::image_write("white_bg_forwards.png")
+  magick::image_write("assets/white_bg_forwards.png")
