dcb2aae50f4e7fedb1d6b925987fb97427d48593
Maëlle Salmon <maelle@itsalocke.com>
2018-06-05 17:19:31 +0200

    keep working on that function cf #6

--- R/create_workshop_certificate.R
+++ R/create_workshop_certificate.R
@@ -1,3 +1,32 @@
+#' Create certificates for all attendees
+#'
+#' @param date Date of the workshop, as a date.
+#' @param workshop Workshop title, character.
+#' @param curriculum Path to the workshop curriculum (.md), character.
+#' @param certifyer Person certifying, character.
+#' @param credentials Credentials of the certifying person, character.
+#' @param attendees Names of attendees, character vector.
+#' @param dir Directory where to output the pdf certificates, character.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' attendees <- c("Marnie Dickinson", "Dr. Marlin Wilderman", "Gail Dietrich",
+#' "Mr. Hall Emard", "Kwame Tillman II", "Mrs. Vessie Gusikowski",
+#' "Burt Purdy", "Kendrick Mohr-Mann", "Stephany Cartwright", "Osvaldo Connelly"
+#' )
+#' date <- structure(17532, class = "Date")
+#' workshop <- "Package development workshop"
+#' curriculum <- system.file("rmarkdown", "templates", "workshop_certificate", "resources", "default_workshop_contents.md", package = "branding")
+#' certifyer <- "Zaire Crooks"
+#' credentials <- "Forwards teaching team member"
+#' dir <- "certificates"
+#' create_workshop_certificates(date, workshop, curriculum, certifyer,
+#' credentials,
+#' attendees,
+#' dir)
+#' }
 create_workshop_certificates <- function(date, workshop, curriculum, certifyer,
                                         credentials,
                                         attendees,
@@ -10,21 +39,21 @@ create_workshop_certificates <- function(date, workshop, curriculum, certifyer,
                           "partly_transparent_forwards.png", package = "branding"),
               file.path(dir, "logo.png"))
 
-    purrr::wak(attendees,
+    purrr::walk2(attendees, 1:length(attendees),
                create_workshop_certificate)
 
    file.remove(file.path(dir, "skeleton.Rmd"))
    file.remove(file.path(dir, "logo.png"))
 }
 
-create_workshop_certificate <- function(attendee,
+create_workshop_certificate <- function(attendee, number,
                                         date, workshop,
                                         curriculum, certifyer,
                                         credentials,
-                                        dir,
-                                        number){
+                                        dir){
     rmarkdown::render(input = "skeleton.Rmd",
-                      output_file = snakecase::to_snake_case(paste(workshop, number)),
+                      output_file = snakecase::to_snake_case(paste(workshop,
+                                                                   stringr::str_pad(number, 2, pad = "0"), ".png")),
                       output_dir = dir,
                       params = list(date = date,
                                     worshop = workshop,
